8795901e248a876a2b3e89172add091b248ec7a1
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-03-12 13:49:34 +0200

    create functions collection files

---
+++ server/utils/index.js
@@ -0,0 +1,8 @@
+const loginSchema = require('./validation/loginSchema');
+const registerSchema = require('./validation/registerSchema');
+
+module.exports = {
+  loginSchema,
+  registerSchema,
+
+};
